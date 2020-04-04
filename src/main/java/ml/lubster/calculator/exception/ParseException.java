7c95117737db12938e3d79db14b69b960a92fb41
Lubchik <lubchik.14@gmail.com>
2020-04-04 23:38:27 +0300

    Added parsing Exception

---
+++ src/main/java/ml/lubster/calculator/exception/ParseException.java
@@ -0,0 +1,14 @@
+package ml.lubster.calculator.exception;
+
+public class ParseException extends Exception {
+    private String message;
+
+    public ParseException(String message) {
+        super();
+        this.message = message;
+    }
+
+    public String toString() {
+        return this.message;
+    }
+}
