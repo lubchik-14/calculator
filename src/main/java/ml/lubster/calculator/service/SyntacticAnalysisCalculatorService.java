2d8b020be151a9efa3576a76e0ac8bbd8f9c668f
Lubchik <lubchik.14@gmail.com>
2020-04-04 23:48:04 +0300

    Added a calculator (method of syntactic analysis)

---
+++ src/main/java/ml/lubster/calculator/service/SyntacticAnalysisCalculatorService.java
@@ -0,0 +1,169 @@
+package ml.lubster.calculator.service;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import ml.lubster.calculator.exception.ParseException;
+import ml.lubster.calculator.model.Calculation;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotNull;
+
+@Service
+public class SyntacticAnalysisCalculatorService implements CalculatorService {
+    private Token currentToken;
+    private int currentIndex;
+
+    private enum TokenType {
+        NONE, DELIMITER, NUMBER
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    @Setter
+    private class Token {
+        private String value = "";
+        private TokenType tokenType = TokenType.NONE;
+    }
+
+    public Calculation evaluate(@NotNull(message = "{invalid.no-expression}") String exp) throws ParseException {
+        double result;
+        currentIndex = 0;
+        getNextToken(exp);
+        if (isEnd(currentToken))
+            throw new ParseException("{invalid.no-expression}");
+        result = AddOrSub(exp);
+        if (!isEnd(currentToken))
+            throw new ParseException("{invalid.syntax}");
+        return new Calculation(exp, result);
+    }
+
+    private void getNextToken(String exp) {
+        this.currentToken = new Token();
+        if (currentIndex == exp.length()) {
+            setEnd(currentToken);
+            return;
+        }
+
+        while (currentIndex < exp.length() && Character.isWhitespace(exp.charAt(currentIndex))) {
+            ++currentIndex;
+        }
+
+        if (currentIndex == exp.length()) {
+            setEnd(currentToken);
+            return;
+        }
+
+        if (isDelimiter(exp.charAt(currentIndex))) {
+            currentToken.setValue(currentToken.getValue() + exp.charAt(currentIndex));
+            currentIndex++;
+            currentToken.setTokenType(TokenType.DELIMITER);
+        } else if (Character.isDigit(exp.charAt(currentIndex))) {
+            while (!isDelimiter(exp.charAt(currentIndex))) {
+                currentToken.setValue(currentToken.getValue() + exp.charAt(currentIndex));
+                currentIndex++;
+                if (currentIndex >= exp.length()) {
+                    break;
+                }
+            }
+            currentToken.setTokenType(TokenType.NUMBER);
+        } else {
+            setEnd(currentToken);
+        }
+    }
+
+    private double AddOrSub(String exp) throws ParseException {
+        double result;
+        double part;
+        result = MulOrDev(exp);
+        char at;
+        while ((at = currentToken.getValue().charAt(0)) == '+' ||
+                at == '-') {
+            getNextToken(exp);
+            part = MulOrDev(exp);
+            switch (at) {
+                case '-':
+                    result -= part;
+                    break;
+                case '+':
+                    result += part;
+            }
+        }
+        return result;
+    }
+
+    private double MulOrDev(String exp) throws ParseException {
+        double result;
+        double part;
+        char at;
+        result = defineUnary(exp);
+        while ((at = currentToken.getValue().charAt(0)) == '*' || at == '/') {
+            getNextToken(exp);
+            part = defineUnary(exp);
+            switch (at) {
+                case '*':
+                    result *= part;
+                    break;
+                case '/':
+                    if (part == 0.0)
+                        throw new ParseException("{invalid.zero}");
+                    result /= part;
+            }
+        }
+        return result;
+    }
+
+    private double defineUnary(String exp) throws ParseException {
+        double result;
+        String at = " ";
+        if ((currentToken.getTokenType().equals(TokenType.DELIMITER)) && currentToken.getValue().equals("+") ||
+                currentToken.getValue().equals("-")) {
+            at = currentToken.getValue();
+            getNextToken(exp);
+        }
+        result = processBrackets(exp);
+        if (at.equals("-"))
+            result = -result;
+        return result;
+    }
+
+    private double processBrackets(String exp) throws ParseException {
+        double result;
+        if (currentToken.getValue().equals("(")) {
+            getNextToken(exp);
+            result = AddOrSub(exp);
+            if (!currentToken.getValue().equals(")"))
+                throw new ParseException("{invalid.parentheses}");
+            getNextToken(exp);
+        } else
+            result = getNumber(exp);
+        return result;
+    }
+
+    private double getNumber(String exp) throws ParseException {
+        double result;
+        if (currentToken.getTokenType() == TokenType.NUMBER) {
+            try {
+                result = Double.parseDouble(currentToken.getValue());
+            } catch (NumberFormatException exc) {
+                throw new ParseException("{invalid.syntax}");
+            }
+            getNextToken(exp);
+        } else {
+            throw new ParseException("{invalid.syntax}");
+        }
+        return result;
+    }
+
+    private boolean isDelimiter(char charAt) {
+        return (" +-/*=()".indexOf(charAt)) != -1;
+    }
+
+    private boolean isEnd(Token token) {
+        return token.getValue().equals("\0");
+    }
+
+    private void setEnd(Token token) {
+        token.setValue("\0");
+    }
+}
