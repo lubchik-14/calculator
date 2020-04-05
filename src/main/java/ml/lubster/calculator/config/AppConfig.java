9d8c642fb7fd27594890bef4eaa964da0443cf32
Lubchik <lubchik.14@gmail.com>
2020-04-05 15:50:09 +0300

    Set a favicon

--- src/main/java/ml/lubster/calculator/config/AppConfig.java
+++ src/main/java/ml/lubster/calculator/config/AppConfig.java
@@ -1,13 +1,11 @@
 package ml.lubster.calculator.config;
 
 import ml.lubster.calculator.model.Calculation;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
