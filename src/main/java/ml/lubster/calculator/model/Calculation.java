837c0cba3bea5ffa08399ba0acc3b7b61f7ff42b
Lubchik <lubchik.14@gmail.com>
2020-04-06 07:35:14 +0300

    Added calculation statuses

--- src/main/java/ml/lubster/calculator/model/Calculation.java
+++ src/main/java/ml/lubster/calculator/model/Calculation.java
@@ -6,7 +6,18 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Calculation {
-    private String expression;
+    private String expression = "";
     private double result;
-}
+    private CalculationStatus status = CalculationStatus.NEW;
 
+    public enum CalculationStatus {
+        NEW, IN_PROGRESS, DONE, ERROR
+    }
+
+    public String resultAsString() {
+        if (result == (long) result)
+            return String.format("%d", (long) result);
+        else
+            return String.format("%s", result);
+    }
+}
